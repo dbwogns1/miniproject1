ee55928dfdda3d973fb16f28f4ddde1963cd2f98
pado8 <floe9235@gmail.com>
2025-02-14 18:16:55 +0900

    미니프로젝트 메인페이지 진행중

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('#header .dropdown-btn').addEventListener('click', function () {
+  document.querySelector('#header .dropdown_btn').addEventListener('click', function () {
     let tg = this;
     if (!(tg.classList.contains('active'))) {
       tg.classList.add('active');
-      document.querySelector('#header .nav-dropdown').classList.add('on');
+      document.querySelector('#header .nav_dropdown').classList.add('on');
     } else {
       tg.classList.remove('active');
-      document.querySelector('#header .nav-dropdown').classList.remove('on');
+      document.querySelector('#header .nav_dropdown').classList.remove('on');
     }
   });
 });
